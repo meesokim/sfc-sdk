69aacc906f04cafc5acc0c442429054131c58efb
forest.belton <forest.belton@6656a202-2174-11df-8467-c9b878d02f05>
2010-02-25 02:00:00 +0000

    Updated tools/snesum.c to support more features. Details in file.

--- tools/snesum.c
+++ tools/snesum.c
@@ -19,10 +19,17 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * File:        snesum.c
- * Description: An application to compute and update a checksum for a SNES ROM
- *              file.
+ * File:           snesum.c
+ * Description:    An application to compute and update a checksum for a SNES ROM
+ *                 file.
  * Changelog:
+ *  --Date:        Feb 24th, 2010
+ *  --Description: Cleaned up a bit of the indentation for consistency. Added
+ *                 comprehensive support for program parameters. This allows
+ *                 for HiROM ROMs to be used. Also added a usage message which
+ *                 displays information about the program.
+ *  --Author:      Forest Belton (apples)
+ *
  *  --Date:        Feb 24th, 2010
  *  --Description: Initial revision.
  *  --Author:      Forest Belton (apples)
@@ -32,6 +39,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define die(msg) do {                \
   fprintf(stderr, "error: %s", msg); \
@@ -42,6 +50,18 @@
 #define HIROM_HEADER          0xffc0
 #define OFFSET_CHECKSUM       0x001c
 
+void usage(void)
+{
+  fprintf(stderr, "SNESum v0.02 by Forest Belton\n");
+  fprintf(stderr, "usage: snesum [opts] [rom]\n");
+  fprintf(stderr,
+    "[opts] -l: Treat the ROM as a LoROM. Enabled by default.\n"
+    "       -h: Treat the ROM as a HiROM.\n"
+    "       -u: Print this message.\n");
+  fprintf(stderr, "[rom]  The ROM file to update the checksum of.\n");
+  exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[])
 {
   FILE        *in;
@@ -49,11 +69,31 @@ int main(int argc, char *argv[])
   unsigned int addr, i, is_lorom = 1;
   int          c;
   
-  if(argc != 2)
-    die("incorrect parameters");
+  /* Parse program parameters. */
+  while((c = getopt(argc, argv, "lhu")) != -1) {
+    switch(c) {
+      case 'h':
+        is_lorom = 0;
+        break;
+      
+      case 'l':
+        is_lorom = 1;
+        break;
+      
+      case 'u':
+        usage();
+        break;
+    }
+  }
   
-  if(!(in = fopen(argv[1], "rb+")))
+  /* Display usage if insufficient parameters. */
+  if((argc != 2) || (argv[optind] == NULL)) {
+    usage();
+  }
+  
+  if(!(in = fopen(argv[optind], "rb+"))) {
     die("failed to open input file");
+  }
   
   /* Determine whether the ROM has a SMC header or not. */
   fseek(in, 0, SEEK_END);
